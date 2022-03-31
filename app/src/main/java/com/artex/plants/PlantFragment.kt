a4e1e0852a9e37dc518694ad3ef7630d5572c750
AlexRyzhickov <makseljoinb@gmail.com>
2022-03-31 12:58:32 +0300

    adding choosing image from gallery

--- app/src/main/java/com/artex/plants/PlantFragment.kt
+++ app/src/main/java/com/artex/plants/PlantFragment.kt
@@ -1,14 +1,20 @@
 package com.artex.plants
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.artex.plants.data.Plant
 import com.artex.plants.data.ScheduleMode
+import com.squareup.picasso.Picasso
 
 
 class PlantFragment : Fragment(R.layout.plant) {
@@ -16,6 +22,7 @@ class PlantFragment : Fragment(R.layout.plant) {
     lateinit var topic: TextView
     lateinit var comment: TextView
     lateinit var date: TextView
+    lateinit var plantImage: ImageView
     private val args: PlantFragmentArgs by navArgs()
     lateinit var plant: Plant
 
@@ -27,6 +34,7 @@ class PlantFragment : Fragment(R.layout.plant) {
         topic = view.findViewById(R.id.plantTopic)
         comment = view.findViewById(R.id.plantComment)
         date = view.findViewById(R.id.plantDate)
+        plantImage = view.findViewById(R.id.plantImage)
         topic.text = plant.name
         comment.text = plant.comment
         date.text = plant.createTime
@@ -45,6 +53,25 @@ class PlantFragment : Fragment(R.layout.plant) {
             val action = PlantFragmentDirections.actionPlantToNotificationsFragment()
             findNavController().navigate(action)
         }
+
+        plantImage.setOnClickListener {
+            openActivityForResult()
+        }
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent = result.data!!
+            val currentUri: Uri? = data!!.data
+            Picasso.get().load(currentUri).into(plantImage)
+        }
+    }
+
+    fun openActivityForResult() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        resultLauncher.launch(intent)
     }
 
 }
